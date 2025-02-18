03b80ae8a00b5024019e90f2d04cd5ba9a0c2ac0
Junaid Arshad Malik <151767417+SPECT3R0@users.noreply.github.com>
2025-02-18 23:10:20 +0500

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import logging
 from interface.cli import run_cli
 from config import load_config
+import asyncio
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='echomitm.log', filemode='w')
 logger = logging.getLogger(__name__)
 
-def main():
+async def main():
     try:
         config = load_config()
         if config:
-            run_cli(config)
+            await run_cli(config)
         else:
             logger.error("Failed to load configuration.")
     except Exception as e:
         logger.exception(f"An error occurred in main: {e}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
