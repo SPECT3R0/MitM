65ebaeed23599a163ab9590700cb3c1454115472
Junaid Arshad Malik <151767417+SPECT3R0@users.noreply.github.com>
2025-02-19 11:49:14 +0500

    Create __init__.py

---
+++ evasion/__init__.py
@@ -0,0 +1,5 @@
+# Empty file or can include package-level imports
+from .traffic_shaping import shape_traffic
+from .encryption_c2 import encrypt_command_control
+from .packet_fragmentation import fragment_packet
+from .decoy_traffic import generate_decoy
